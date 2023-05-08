eb76a0158cdfd959319df981a34fdb3bc4bba845
Andrey Sery <andrey.72006@gmail.com>
2023-05-09 01:04:21 +0300

    Swiper init

---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+import Swiper from "swiper/bundle";
+
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
