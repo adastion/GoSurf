e5e95bbfb62f47a813dc7a46b76b4b6880739dae
Andrey Sery <andrey.72006@gmail.com>
2023-05-09 16:34:59 +0300

    Header plus slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,19 @@
 import Swiper from "swiper/bundle";
 
 const swiper = new Swiper(".swiper", {
-  loop: true,
+  spaceBetween: 30,
+
+  effect: "fade",
+
+  autoplay: {
+    delay: 2000,
+  },
+
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-
   pagination: {
     el: ".swiper-pagination",
-    clickable: true,
   },
 });
