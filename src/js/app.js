8574679d85d383e449f629684132dc6399de4171
Andrey Sery <andrey.72006@gmail.com>
2023-06-14 00:55:48 +0300

    Initializing slider-header

---
+++ src/js/app.js
@@ -0,0 +1,23 @@
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+const swiper = new Swiper(".slider-header", {
+  modules: [Navigation, Pagination],
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  },
+  
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+});
