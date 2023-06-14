08dabda3dacb6a6d78027715c85b07cc1eb68435
Andrey Sery <andrey.72006@gmail.com>
2023-06-15 00:44:20 +0300

    slider-header completed

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,27 @@
-import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper, { Navigation, Pagination } from "swiper";
+const nameCard = [...document.querySelectorAll('.name-slide')].map(item => item.textContent);
 
 const swiperHeader = new Swiper(".slider-header", {
   modules: [Navigation, Pagination],
 
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
-  
+
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
     renderBullet: function (index, className) {
-      return '<span class="' + className + '">' + (index + 1) + "</span>";
+      return `<div class="${className}">
+        <span>0${index + 1}</span>
+        <span class="${className}-name">${nameCard[index]}</span>
+      </div>`;
     },
   },
-  
-  effect: 'fade',
+
+  effect: "fade",
   fadeEffect: {
-    crossFade: true
+    crossFade: true,
   },
 });
